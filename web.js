215ef6022ce5f12f656ee5c40601f7894de80aa0
Ken Zheng <kzheng@ucdavis.edu>
2013-07-07 07:27:21 +0000

    Fixed a bug

--- web.js
+++ web.js
@@ -5,8 +5,8 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   //response.send('Hello World 2!');
   var fs = require('fs')
-  var buffer = new Buffer(fs.readFileSync("index.html"), "utf-8");
-  response.send(buffer.toString('utf-8');
+  //fs.readFileSync("index.html"), "utf-8");
+  response.send(fs.readFileSync("index.html").toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
